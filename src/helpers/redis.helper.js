212bd02cfe6687126874c985ec2fa94e28537de6
Manali Sonar <96368868+Manali1892@users.noreply.github.com>
2022-11-10 00:04:46 +0530

    Add files via upload

---
+++ src/helpers/redis.helper.js
@@ -0,0 +1,44 @@
+const redis = require("redis");
+const client = redis.createClient(process.env.REDIS_URL);
+(async () => { await client.connect(); })
+client.on('error', (err) => console.log('Redis Client Error', err));
+
+
+const setJWT = (key, value) => {
+  return new Promise((resolve, reject) => {
+    try {
+      return client.set(key, value, (err, res) => {
+        if (err) reject(err);
+        resolve(res);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const getJWT = (key) => {
+  return new Promise((resolve, reject) => {
+    try {
+      client.get(key, (err, res) => {
+        if (err) reject(err);
+        resolve(res);
+      });
+    } catch (error) {
+      reject(error);
+    }
+  });
+};
+
+const deleteJWT = (key) => {
+  try {
+    client.del(key);
+  } catch (error) {
+    console.log(error);
+  }
+};
+module.exports = {
+  setJWT,
+  getJWT,
+  deleteJWT,
+};
